b002bbb87fe40bd89c0f053e9fa8314990c6e3b0
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2024-11-09 23:50:18 +0000

    New files

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+// Establish connection to database
+
+// Define database server, username, password, and database name
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'vividlydb';
+
+// Create a new MySQLi connection object using the specified credentials
+$conn = new  mysqli($servername, $username, $password, $dbname);
+
+
+// Check if the connection was successful
+if($conn->connect_error){
+  die("Connection failed");
+}else{
+  // echo "Connection successful";
+}
+?>
