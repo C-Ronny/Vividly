38195681971e74f1ce879613d706042c08781a1e
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2024-11-14 14:29:39 +0000

    Minor chnages

---
+++ actions/login_user.php
@@ -0,0 +1,62 @@
+<?php
+include '../db/config.php';
+// Include config files for database connection
+
+include '../util/error_config.php';
+
+// Start session
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = trim($_POST['email']);
+    $password = trim($_POST['psw']);
+
+    // Check if email and password are not empty
+    if (empty($email) || empty($password)) {
+        echo "<script>alert('Please fill in all fields'); window.location.href='../view/login.php';</script>";
+        exit();
+    }
+
+    // Prepare a query to check if the user exists
+    $query = 'SELECT user_id, `password`, fname, lname, email, `role` FROM `users` WHERE email = ?';
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param('s', $email);
+    $stmt->execute();
+    $results = $stmt->get_result();
+
+    if ($results->num_rows > 0) {
+        // Fetch user data from the result set
+        $row = $results->fetch_assoc();
+        $user_id = $row['user_id'];
+        $fname = $row['fname'];
+        $lname = $row['lname'];
+        $email = $row['email'];
+        $userrole = $row['role'];
+        $userpass = $row['password'];
+
+        // Verify password
+        if (password_verify($password, $userpass)) {
+            // Start session and set session variables
+            $_SESSION['user_id'] = $user_id;
+            $_SESSION['email'] = $email;
+            $_SESSION['userrole'] = $userrole;
+
+            // Redirect to appropriate page
+            if ($userrole == 1) {
+                header('Location: view/superuser.php');
+            } else {
+                header('Location: ../view/recipefeed.php');
+            }
+            exit();
+        } else {
+            echo "<script>alert('Invalid login credentials'); window.location.href='../view/login.php';</script>";
+        }
+    } else {
+        echo "<script>alert('User does not exist'); window.location.href='../view/login.php';</script>";
+    }
+
+    // Close statement and connection
+    $stmt->close();
+}
+$conn->close();
+?>
--- view/profile.php
+++ view/profile.php
@@ -4,9 +4,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Vividly | Account</title>
-  <link rel="stylesheet" href="profile.css">
+  <link rel="stylesheet" href="../assets/profile.css">
 </head>
 <body>
+
   
 </body>
 </html>
