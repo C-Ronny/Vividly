4e0d5227e6f2e3e3b5fb57e873a92f8d424fda58
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2024-11-21 22:44:36 +0000

    cooking...

--- view/user_pages/profile.php
+++ view/user_pages/profile.php
@@ -4,10 +4,29 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Vividly | Account</title>
-  <link rel="stylesheet" href="../assets/css/profile.css">
+  <link rel="stylesheet" href="../../assets/css/profile.css">
 </head>
 <body>
+  <!-- Nav Bar -->
+  <nav id="nav">
+    <h2 id="vividly-logo">Vividly</h2>
 
+    <!--Search bar-->
+    <div class="search">
+      <input type="text" placeholder="Search your Boards...">
+    </div>
 
+    <div class="nav-center">
+      <!-- <a class="nav-a" href="#">Boards</a> -->
+      <a class="nav-a" href="../user_pages/profile.php">Logout</a>
+      <a href="profile.php"><img src="../../assets/images/bg1.jpg"></a>
+    </div>
+
+  </nav>
+  <hr id="nav-rule">
+
+  <main>
+
+  </main>
 </body>
 </html>
