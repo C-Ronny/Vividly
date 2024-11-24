2b668ef01f07c9541d24da6ec2e609a4735f53d0
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2024-11-24 00:21:55 +0000

    taking the pots and pans out for profile cooking...

--- view/user_pages/profile.php
+++ view/user_pages/profile.php
@@ -1,32 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Vividly | Account</title>
-  <link rel="stylesheet" href="../../assets/css/profile.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vividly | Account</title>
+    <link rel="stylesheet" href="../../assets/css/profile.css">
 </head>
+
 <body>
-  <!-- Nav Bar -->
-  <nav id="nav">
-    <h2 id="vividly-logo"><a href="../user_pages/landingpage.php">Vividly</a></h2>
+    <!-- Nav Bar -->
+    <nav id="nav">
+        <h2 id="vividly-logo"><a href="../user_pages/landingpage.php">Vividly</a></h2>
 
-    <!--Search bar-->
-    <div class="search">
-      <input type="text" placeholder="Search your Boards...">
-    </div>
+        <div class="nav-center">
+            <!-- <a class="nav-a" href="#">Boards</a> -->
+            <a class="nav-a" href="../user_pages/profile.php">Logout</a>
+            <a href="profile.php"><img src="../../assets/images/bg1.jpg"></a>
+        </div>
 
-    <div class="nav-center">
-      <!-- <a class="nav-a" href="#">Boards</a> -->
-      <a class="nav-a" href="../user_pages/profile.php">Logout</a>
-      <a href="profile.php"><img src="../../assets/images/bg1.jpg"></a>
-    </div>
+    </nav>
+    <hr id="nav-rule">
 
-  </nav>
-  <hr id="nav-rule">
+    <main>
 
-  <main>
-
-  </main>
+    </main>
 </body>
+
 </html>
