726b73a0b0f18dbe6ad1612825e4325c917d9167
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2024-11-27 18:48:23 +0000

    db cooking...

--- db/Vividly.sql
+++ db/Vividly.sql
@@ -29,13 +29,14 @@ CREATE TABLE Boards (
 
 -- Pins Table
 CREATE TABLE Pins (
-    pin_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT ,
-    board_id INT,
-    image_url VARCHAR(255) NOT NULL,
-    `description` TEXT,
-    category_id INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    pin_id INT AUTO_INCREMENT PRIMARY KEY, -- pin id number
+    user_id INT , -- user id number
+    board_id INT,   -- board id number
+    image_url VARCHAR(255) NOT NULL,    -- image url
+    caption VARCHAR(255) NOT NULL, -- title of the pos
+    `description` TEXT, -- description of the post
+    category_id INT,    -- category id number
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
     FOREIGN KEY (board_id) REFERENCES Boards(board_id) ON DELETE CASCADE,
@@ -43,15 +44,15 @@ CREATE TABLE Pins (
 );
 
 -- Comments Table
--- CREATE TABLE Comments (
---     comment_id INT AUTO_INCREMENT PRIMARY KEY,
---     pin_id INT,
---     user_id INT,
---     comment TEXT,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     FOREIGN KEY (pin_id) REFERENCES Pins(pin_id) ON DELETE CASCADE,
---     FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
--- );
+CREATE TABLE Comments (
+    comment_id INT AUTO_INCREMENT PRIMARY KEY,
+    pin_id INT,
+    user_id INT,
+    comment TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (pin_id) REFERENCES Pins(pin_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
+);
 
 -- Likes Table
 CREATE TABLE Likes (
@@ -66,8 +67,8 @@ CREATE TABLE Likes (
 -- Categories Table
 CREATE TABLE Categories (
     category_id INT AUTO_INCREMENT PRIMARY KEY,
-    `name` tinyint(6)  NOT NULL,
-    `description` TEXT
+    `name` tinyint(6)  NOT NULL, -- 6 categories (Art, Design, Fashion, Food, Photography, Travel)
+    `description` TEXT -- Description of each category
 );
 
 -- User Statistics Table
