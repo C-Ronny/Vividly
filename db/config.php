39e965f804351ae7523ceeb51a6977dae9770deb
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2024-12-16 13:38:12 +0000

    ..

--- db/config.php
+++ db/config.php
@@ -1,21 +1,18 @@
 <?php
 
-// Establish connection to database
-
 // Define database server, username, password, and database name
 $servername = 'localhost';
 $username = 'root';
 $password = '';
 $dbname = 'Vividly';
 
-// Create a new MySQLi connection object using the specified credentials
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-
-// Check if the connection was successful
-if($conn->connect_error){
-  die("Connection failed");
-}else{
-  // echo "Connection successful";
+// Check connection
+if ($conn->connect_error) {
+    error_log("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
