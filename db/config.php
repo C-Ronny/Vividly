67ab8c35a3004922b55586b156fa8704faa9c023
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2024-12-17 15:51:56 +0000

    uploading to filezilla

--- db/config.php
+++ db/config.php
@@ -3,8 +3,10 @@
 // Define database server, username, password, and database name
 $servername = 'localhost';
 $username = 'root';
-$password = '';
-$dbname = 'Vividly';
+$password = 'Ronelle-0202731402-rocu';
+// $password = '';
+$dbname = 'webtech_fall2024_ronelle_cudjoe';
+// $dbname = 'Vividly';
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
